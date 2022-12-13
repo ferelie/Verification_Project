bdcefa96c65be79849eaca9dcbd46f1ed13dbe67
abdallah <abdallah.yasser0501@gmail.com>
2022-12-13 11:51:37 +0200

    Added jUnit package and created an initial test class

---
+++ BowlingGameTest.java
@@ -0,0 +1,30 @@
+package Bowling;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BowlingGameTest {
+
+    @org.junit.jupiter.api.Test
+    void getCurrentFrame() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCurrentBall() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void scoreBall() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBallScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isStrike() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isSpare() {
+    }
+}
